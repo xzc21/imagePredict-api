99a0054bced528e19bbd34311206d93146201ef8
xzc21 <itswilltian@gmail.com>
2022-01-11 01:07:09 -0500

    database

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const image = require('./controllers/image');
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-adjacent-53739',
       port : 5432,
       user : 'postgres',
       password : 'test',
